620c2256cb4977105ff30ae510d6fdce03a50b92
Supisara Prasittinawa <supisara_2540@hotmail.com>
2019-08-22 02:19:30 +0700

    Layout

--- app/src/main/java/buu/informatics/s59160134/carpark/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160134/carpark/MainActivity.kt
@@ -8,9 +8,13 @@ import buu.informatics.s59160134.carpark.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val carInfo1: CarInfo = CarInfo("","","","ว่าง")
+    private val carInfo2: CarInfo = CarInfo("","","","ว่าง")
+    private val carInfo3: CarInfo = CarInfo("","","","ว่าง")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.carInfo1 = carInfo1
+        binding.carInfo2 = carInfo2
+        binding.carInfo3 = carInfo3
     }
 }
